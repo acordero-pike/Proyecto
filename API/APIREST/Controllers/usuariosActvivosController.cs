73746afc51e48db3e8e08f5e96a8b40f2136eac0
Lourdes Aguirre <loumaagui@gmail.com>
2021-07-14 15:40:28 -0600

    endpoint post para activar usuario añadido

---
+++ API/APIREST/Controllers/usuariosActvivosController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usuariosActvivosController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult Post([FromBody] Models.Usuario modelo)
+        {
+            using (Models.proyectoCursosContext db = new Models.proyectoCursosContext())
+            {
+                Models.Usuario usuario = db.Usuarios.Find(modelo.IdUsuario);
+                usuario.Status = true;
+
+                db.Entry(usuario).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                db.SaveChanges();
+            }
+            return Ok("Usuario activo");
+        }
+    }
+}
