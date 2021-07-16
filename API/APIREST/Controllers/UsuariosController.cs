7bee57d5bc29265f1deb4c87b00f8be05d1237e7
Albin <you@example.comalbincordero2000@gmail.com  git config --global user.email you@example.comalbincordero2000@gmail.com>
2021-07-16 16:50:42 -0600

    160721

---
+++ API/APIREST/Controllers/UsuariosController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+ 
+//albin cordero 13/07/2021
+namespace APIREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+
+
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult Get()
+        {
+            using (Models.ProyectocrsContext db = new Models.ProyectocrsContext())
+            {
+                var usuario = (from d in db.Usuarios
+                                select d).ToList();
+                return Ok(usuario);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetUsuario(int id)
+        {
+            using (Models.ProyectocrsContext db = new Models.ProyectocrsContext())
+            {
+                var usuario = db.Usuarios.Find(id);
+
+                if (usuario == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(usuario);
+            }
+        }
+
+        [HttpGet("{Correo},{Contraseña}")]
+        public ActionResult validar(string Correo, string Contraseña)
+        {
+            using (Models.ProyectocrsContext db = new Models.ProyectocrsContext())
+            {
+                
+
+                var query = db.Usuarios.Where(a => a.Correo==Correo && a.Contraseña == Contraseña).Select(g => new { ID = g.IdUsuario, Nombre = g.Nombre + ' '+g.Apellido }).ToList();
+             
+                if (query.Count()<1)
+                {
+                    var query1 = new { status = "400" };
+                    // creamos un listado de peticion
+                    return Ok(query1);
+                }
+
+                return Ok(query);
+            }
+        }
+    }
+
+}
--- API/APIREST/Startup.cs
+++ API/APIREST/Startup.cs
@@ -1,21 +1,22 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace APIREST
 {
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,12 +27,27 @@ namespace APIREST
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin", builder =>
+                    builder.AllowAnyHeader()
+                           .AllowAnyMethod()
+                           .AllowAnyOrigin()
+                );
+            });
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "APIREST", Version = "v1" });
             });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer();
+
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,7 +63,7 @@ namespace APIREST
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors("AllowSpecificOrigin");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
