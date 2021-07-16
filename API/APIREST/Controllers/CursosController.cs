d58507fccf5efa99944fbb72b3cacacfadf6799d
DeboraVasty <vasty0295@gmail.com>
2021-07-16 16:03:45 -0600

    Crear Cursos y Modificar

---
+++ API/APIREST/Controllers/CursosController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CursosController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            using (Models.apiContext db = new Models.apiContext())
+            {
+                var cursos = (from d in db.Cursos
+                              select d).ToList();
+                return Ok(cursos);
+            }
+
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Models.Solicitudes.CursoSolicitud modelo)
+        {
+            using (Models.apiContext db = new Models.apiContext())
+            {
+                Models.Curso cursos = new Models.Curso();
+                cursos.Nombre = modelo.Nombre;
+                cursos.Descripcion = modelo.Descripcion;
+                cursos.Costo = modelo.Costo;
+                cursos.IdInstructor = modelo.IdInstructor;
+                cursos.Duracion = modelo.Duracion;
+
+                db.Cursos.Add(cursos);
+                db.SaveChanges();
+
+            }
+            return Ok("El curso se añadió correctamente");
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Models.Curso modelo)
+        {
+            using (Models.apiContext db = new Models.apiContext())
+            {
+                Models.Curso cursos = db.Cursos.Find(modelo.IdCurso);
+                cursos.Nombre = modelo.Nombre;
+                cursos.Descripcion = modelo.Descripcion;
+                cursos.Costo = modelo.Costo;
+                cursos.IdInstructor = modelo.IdInstructor;
+                cursos.Duracion = modelo.Duracion;
+
+                db.Entry(cursos).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                db.SaveChanges();
+
+            }
+            return Ok("El curso se Actualizó correctamente");
+        }
+        [HttpDelete]
+        public ActionResult Delete([FromBody] Models.Curso modelo)
+        {
+            using (Models.apiContext db = new Models.apiContext())
+            {
+                bool validar;
+                var query = (from r in db.Cursos where r.IdCurso == modelo.IdCurso select r).Count();
+
+                if(query==0)
+                {
+                    validar = false;
+                }
+                else
+                {
+                    validar = true;
+                }
+
+                if(validar==true)
+                {
+                     Models.Curso cursos = db.Cursos.Find(modelo.IdCurso);
+                     db.Cursos.Remove(cursos);
+                     db.SaveChanges();
+                    return Ok("El curso se Eliminó correctamente");
+
+                }
+                else
+                {
+                    return Ok("El curso no existe");
+                }
+               
+            }
+            
+        }
+    }
+}
---
+++ API/APIREST/Models/DatosInstructor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace APIREST.Models
+{
+    public partial class DatosInstructor
+    {
+        public DatosInstructor()
+        {
+            Cursos = new HashSet<Curso>();
+        }
+
+        public int IdInstructor { get; set; }
+        public string Estudios { get; set; }
+        public string Certificacion { get; set; }
+        public string ExperienciaLab { get; set; }
+        public int CuentaBancaria { get; set; }
+        public int Usuario { get; set; }
+
+        public virtual ICollection<Curso> Cursos { get; set; }
+    }
+}
