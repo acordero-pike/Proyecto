9ba13051daca2ef1396fb00096ed9a291602b6d7
Lourdes Aguirre <loumaagui@gmail.com>
2021-07-15 21:27:07 -0600

    vista registro usuarios creada

---
+++ Vistas/js/global.js
@@ -0,0 +1,5 @@
+const URL = "https://localhost:44364/api/";
+const headers = {
+    'Accept' : "application/json",
+    "Content-Type": "application/json",
+  };
---
+++ Vistas/js/users.js
@@ -0,0 +1,5 @@
+const name = document.getElementById("fname");
+const lastname = document.getElementById("lname");
+const email = document.getElementById("email");
+const phone = document.getElementById("mob");
+const pass = document.getElementById("pass");
